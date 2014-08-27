1a2756466f74500653f6f2745383e574d0fdcafc
YOUR NAME <YOUR@EMAIL.com>
2014-08-27 02:56:35 -0500

    Adds imageDisplay updater protocol

---
+++ photoArchivingIpad/ImageDisplayStoryUpdater.h
@@ -0,0 +1,13 @@
+//
+//  ImageDisplayStoryUpdater.h
+//  photoArchivingIpad
+//
+//  Created by Anthony Forsythe on 8/27/14.
+//  Copyright (c) 2014 Tonyf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol ImageDisplayStoryUpdater <NSObject>
+
+@end
